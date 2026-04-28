e8a5eb7ae1d6804265aad34fec46495353fb5012
ShyamNarayanan1974 <231001200@rajalakshmi.edu.in>
2026-04-28 18:04:52 +0530

    movie.py

---
+++ movie.py
@@ -0,0 +1,8 @@
+class Movie:
+    def __init__(self, movieId, movieTitle, genre, language, rating, duration):
+        self.movieId = movieId
+        self.movieTitle = movieTitle
+        self.genre = genre
+        self.language = language
+        self.rating = rating
+        self.duration = duration
