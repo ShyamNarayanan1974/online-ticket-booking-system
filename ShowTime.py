13fc568dcb03a86f5d6f52fb8091487774ab3ac4
ShyamNarayanan1974 <231001200@rajalakshmi.edu.in>
2026-04-28 18:07:02 +0530

    Showtime.py

---
+++ ShowTime.py
@@ -0,0 +1,8 @@
+class Showtime:
+    def __init__(self, showtimeId, movie, screen, startTime, endTime, pricing):
+        self.showtimeId = showtimeId
+        self.movie = movie        
+        self.screen = screen      
+        self.startTime = startTime
+        self.endTime = endTime
+        self.pricing = pricing
